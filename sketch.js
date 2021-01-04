179af0e9d1f03918d3bf47fc9b835be247753186
nabah03 <73787185+nabah03@users.noreply.github.com>
2021-01-04 10:37:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-var car,wall;
-var speed,weight;
+var wall,thickness;
+var bullet,speed,weight;
 
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
 
-  speed=random(55,90);
-  weight=random(400,1500);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83)
 
   car=createSprite(50,200,50,50);
   car.shapeColor = "green";
 
-  wall=createSprite(1500,200,60,height/2);
+  wall=createSprite(1200,200, thickness, height/2);
   color(80,80,80);
   wall.shapeColor = "green";
 
@@ -33,9 +34,39 @@ if(deformation<100)
 {
   car.shapeColor=color(0,255,0)
 }
+if(hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+}
+if (damage>10)
+{
+  wall.shapeColor=color(255,0,0);
 }
 
+if(damage<10)
+{
+  wall.shapeColor = color(0,255,0)
+}
+
+
+
+
+}
 function draw() {
   background(255,255,255);
   drawSprites();
 }
+function hasCollided(lbullet,lwall)
+{
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdage=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true 
+  }
+  return false ;
+}
+
+
+
